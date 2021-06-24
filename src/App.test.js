0f8a3e8e9faae2122cc4b8bb0d0560d4c10f6043
eg-bond <eg-bond@yandex.ru>
2021-06-24 16:31:41 +0300

    some comments added

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
+  // render отрисовывает компоненту и возвращает методы для взаимодействия с ним
   const { getByText } = render(<App />);
+  // getByText ищет элемент по тексту внутри него (при помощи регулярок)
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  screen.debug();
 });
